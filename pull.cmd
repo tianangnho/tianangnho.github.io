a4d10dcbb0c6de6ab3c3b2b738c4b0466760c6e4
tianangnho <169327685+congtynhuomphulong@users.noreply.github.com>
2026-08-16 05:45:26 +0700

    update diary

--- pull.cmd
+++ pull.cmd
@@ -1,1 +1,11 @@
+@echo off
 git pull --rebase
+IF ERRORLEVEL 1 (
+    echo Rebase failed, fallback call git pull...
+    git pull
+	IF ERRORLEVEL 1 (
+	echo Pull --rebase sucessful.
+	)
+) ELSE (
+    echo Pull --rebase sucessful.
+)
